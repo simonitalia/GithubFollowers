e150d5a8ed3c4cdd12cb5e92075d2f4d210d6f5c
Simon Italia <sitalia@magicaltomato.com>
2020-01-22 23:43:37 +1100

    //SearchVCAndCustomAlert changes covered in: - Video 1

---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,41 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Simon Italia on 1/22/20.
+//  Copyright © 2020 SDI Group Inc. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    //setup base UIButton object with its initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //add GFButton customization ontop of underlying UIButton
+        configure()
+    }
+    
+    //MARK: UIButtom Required init
+    //used for initialization in storyboard implementations, not required in this (non-stpryboard) app, but init must be present in UIButton sublass
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: GFButton customizations
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero) //set to .zero since this will be set via auto layout
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline) //bold
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ GithubFollowers/GFTextField.swift
@@ -0,0 +1,47 @@
+//
+//  GFTextField.swift
+//  GithubFollowers
+//
+//  Created by Simon Italia on 1/22/20.
+//  Copyright © 2020 SDI Group Inc. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    //required init used for storyboard initialization
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: Custom GFTextField configuration
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+            //use UISemantics github app to see how system colors look in light and dark modes
+        
+        textColor = .label //whtie text on black background, black text on white background
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true //shrink to fit text
+        minimumFontSize = 12 //don't shrink text beyond 12
+        
+        placeholder = "Enter username"
+        
+    }
+    
+    
+    
+    
+
+}
