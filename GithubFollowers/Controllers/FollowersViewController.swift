e1fb2e143498716d55cec3f13c8fe26c37707806
Simon Italia <sitalia@magicaltomato.com>
2020-01-26 14:50:33 +1100

    CollectionView changes in: - Video 5
    CollectionView changes not in video (in video, this method was moved to a new Helper class):
    - UICollectionViewFlowLayout extension updated with method to createCollectionViewFlowLayout(view: ), moved out of FollowersVC .

--- GithubFollowers/Controllers/FollowersViewController.swift
+++ GithubFollowers/Controllers/FollowersViewController.swift
@@ -37,7 +37,8 @@ class FollowersViewController: UIViewController {
     
     
     func fireGetFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { [unowned self] (result) in
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] (result) in
+            guard let self = self else { return }
             
             switch result {
             case .success(let followers):
@@ -55,41 +56,19 @@ class FollowersViewController: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        
     }
     
     func createCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCollectionViewFlowLayout())
+        //create flow layout object
+        let layout = UICollectionViewFlowLayout().createCollectionViewFlowLayout(view: view)
+        
+        //create collectionView (with layout obejct)
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
         view.addSubview(collectionView)
+        
+        //configure collectionView
         collectionView.backgroundColor = .systemBackground
-        collectionView.register(FollowerCollectionViewCell.self, forCellWithReuseIdentifier: FollowerCollectionViewCell.reuseIdentifier)
-            //register custom cell subclass
-    }
-    
-    func createCollectionViewFlowLayout() -> UICollectionViewFlowLayout {
-        
-        //configure width of cells / columns based on 3 column layout
-        let numberOfColumns: CGFloat = 3
-        
-        //set properies to calculate cell width
-        let viewWidth = view.bounds.width //CGFloat value
-        let edgeInsetsPadding: CGFloat = 12
-        let minimumCellSpacing: CGFloat = 10 * 2
-            //x2 to account for spacing between first / left most column and middle column, + the last / right most column and middle column (3 columns total)
-        
-        //calculate cell width
-        let availableWidthForCell = viewWidth - (edgeInsetsPadding * 2) - minimumCellSpacing
-            //edgeInsets *2 to allow for both left + right edges of screen
-        
-        //set cell width
-        let cellWidth = availableWidthForCell / numberOfColumns
-        
-        let layout = UICollectionViewFlowLayout()
-//        layout.sectionInset = UIEdgeInsets(top: edgeInsetsPadding, left: edgeInsetsPadding, bottom: edgeInsetsPadding, right: edgeInsetsPadding)
-        layout.sectionInset = layout.setEdgeInsets(to: edgeInsetsPadding)
-        layout.itemSize = CGSize(width: cellWidth, height: cellWidth + 40)
-        
-        return layout
+        collectionView.register(FollowerCollectionViewCell.self, forCellWithReuseIdentifier: FollowerCollectionViewCell.reuseIdentifier) //registers our custom cell subclass
     }
     
     func configureCollectionViewCell() {
--- GithubFollowers/Extensions/UICollectionViewFlowLayout+Extension.swift
+++ GithubFollowers/Extensions/UICollectionViewFlowLayout+Extension.swift
@@ -10,6 +10,34 @@ import UIKit
 
 extension UICollectionViewFlowLayout {
     
+    //set flow layout
+    func createCollectionViewFlowLayout(view: UIView) -> UICollectionViewFlowLayout {
+        
+        //configure width of cells / columns based on 3 column layout
+        let numberOfColumns: CGFloat = 3
+        
+        //set properies to calculate cell width
+        let viewWidth = view.bounds.width //CGFloat value
+        let edgeInsetsPadding: CGFloat = 12
+        let minimumCellSpacing: CGFloat = 10 * 2
+            //x2 to account for spacing between first / left most column and middle column, + the last / right most column and middle column (3 columns total)
+        
+        //calculate cell width
+        let availableWidthForCell = viewWidth - (edgeInsetsPadding * 2) - minimumCellSpacing
+            //edgeInsets *2 to allow for both left + right edges of screen
+        
+        //set cell width
+        let cellWidth = availableWidthForCell / numberOfColumns
+        
+        let layout = UICollectionViewFlowLayout()
+//        layout.sectionInset = UIEdgeInsets(top: edgeInsetsPadding, left: edgeInsetsPadding, bottom: edgeInsetsPadding, right: edgeInsetsPadding)
+        layout.sectionInset = layout.setEdgeInsets(to: edgeInsetsPadding) //replaces above line
+        layout.itemSize = CGSize(width: cellWidth, height: cellWidth + 40)
+        
+        return layout
+    }
+    
+    //set edgeInsets
     func setEdgeInsets(to edgeInset: CGFloat) -> UIEdgeInsets {
         return UIEdgeInsets(top: edgeInset, left: edgeInset, bottom: edgeInset, right: edgeInset)
     }
